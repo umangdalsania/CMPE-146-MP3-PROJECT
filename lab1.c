41736a42fd8aeab477e56a5725aeab02e7f3b5e1
Vidushi1790 <vidushi1790@gmail.com>
2021-02-16 12:33:15 -0800

    updated file for demo

--- lab1.c
+++ lab1.c
@@ -1,25 +1,6 @@
-new change
 #include <stdio.h>
 
-#include FreeRTOS.h
-#include task.h
-
-#include cpu_utilization_task.h
-
-/**
- * This POSIX based FreeRTOS simulator is based on:
- * https://github.com/linvis/FreeRTOS-Sim
- *
- * Do not use for production!
- * There may be issues that need full validation of this project to make it production intent.
- * This is a great teaching tool though :)
- */
 int main(void) {
-  xTaskCreate(cpu_utilization_print_task, cpu, 1, NULL, PRIORITY_LOW, NULL);
-
-  puts(Starting FreeRTOS Scheduler ..... rn);
-  vTaskStartScheduler();
-
+  printf("Git Merge Conflict!\n");
   return 0;
 }
-
